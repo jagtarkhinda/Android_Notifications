e968895833546ba1dca165083ea890fe197be68d
Jagtar Singh <jagtar.ca@gmail.com>
2019-09-20 19:17:29 -0400

    send notification on specific time

--- app/src/main/java/com/jagtar/androidnotificationbasics/MainActivity.java
+++ app/src/main/java/com/jagtar/androidnotificationbasics/MainActivity.java
@@ -1,20 +1,26 @@
 package com.jagtar.androidnotificationbasics;
 
+import android.app.AlarmManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.icu.util.Calendar;
 import android.os.Build;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+
+import java.util.Date;
+import java.util.Timer;
+
 public class MainActivity extends AppCompatActivity {
     public static final String CHANNEL_ID = "notf1";
+    Calendar setTime = Calendar.getInstance();
+    private AlarmManager myAlarm;
+    private PendingIntent pendingIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,35 +38,53 @@ public class MainActivity extends AppCompatActivity {
             NotificationManager notificationManager = getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
         }
+        ////////////////////////////////
 
+        //setting time at which we need the notification
+        setTime.set(Calendar.HOUR_OF_DAY,19);
+        setTime.set(Calendar.MINUTE, 9);
 
-
-    }
-    public void notifyMe(View view){
-
-        // Create an explicit intent for an Activity in your app
-        //it is used when user tap on the notification
-        Intent intent = new Intent(this, MainActivity.class);
+        //intent to use to broadcast the alarm request
+        Intent intent = new Intent(this, Notification_Receive.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        //Building a notification using notification builder
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentTitle("Thie is my first android notification")
-                .setContentText("Hello Android from top bar")
-                //set style is optional to modify the appearance
-                .setStyle(new NotificationCompat.BigTextStyle().bigText("More to add in notification"))
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true);
+        // ALARM MANAGER (setting up alarm)
+        myAlarm = (AlarmManager) getSystemService(ALARM_SERVICE);
+        myAlarm.setRepeating(AlarmManager.RTC_WAKEUP,setTime.getTimeInMillis(),AlarmManager.INTERVAL_DAY,pendingIntent);
 
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-
-        // notificationId is a unique int for each notification that you must define
-        notificationManager.notify(12, builder.build());
+        ///////////
 
     }
+
+
+//    //use this to get notification on button press
+//    public void notifyMe(View view){
+//
+//        // Create an explicit intent for an Activity in your app
+//        //it is used when user tap on the notification
+//        Intent intent = new Intent(this, MainActivity.class);
+//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+//
+//        //Building a notification using notification builder
+//        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+//                .setSmallIcon(R.drawable.ic_launcher_background)
+//                .setContentTitle("Thie is my first android notification")
+//                .setContentText("Hello Android from top bar")
+//                //set style is optional to modify the appearance
+//                .setStyle(new NotificationCompat.BigTextStyle().bigText("More to add in notification"))
+//                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+//                .setContentIntent(pendingIntent)
+//                //remove the notification when user tap on it
+//                .setAutoCancel(true);
+//
+//        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+//
+//        // notificationId is a unique int for each notification that you must define
+//        notificationManager.notify(12, builder.build());
+//
+//    }
 
 
 }
---
+++ app/src/main/java/com/jagtar/androidnotificationbasics/Notification_Receive.java
@@ -0,0 +1,44 @@
+package com.jagtar.androidnotificationbasics;
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+
+import static com.jagtar.androidnotificationbasics.MainActivity.CHANNEL_ID;
+/* put
+* <uses-permission android:name="com.andorid.alarm.permission.SET_ALARM" />
+* and
+*  <receiver android:name=".Notification_Receive" />
+*  in manifest file
+* */
+//extends broadcast receiver to receive broadcasts
+public class Notification_Receive extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+       // public static final String CHANNEL_ID = "notf1";
+        Intent intente = new Intent(context, MainActivity.class);
+        intente.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Thie is my first android notification")
+                .setContentText("Hello Android from top bar")
+                //set style is optional to modify the appearance
+                .setStyle(new NotificationCompat.BigTextStyle().bigText("More to add in notification"))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                //remove the notification when user tap on it
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(12, builder.build());
+    }
+}
